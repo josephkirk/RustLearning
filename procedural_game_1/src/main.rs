4c09951b984a025e8137a0b1fb092745c6ba2975
Nguyen Phi Hung <josephkirk.art@gmail.com>
2024-07-17 10:00:18 +0700

    update procedural game

--- procedural_game_1/src/main.rs
+++ procedural_game_1/src/main.rs
@@ -4,23 +4,26 @@ use bevy::prelude::*;
 use bevy::prelude::Srgba;
 use rand::prelude::*;
 use std::collections::HashMap;
-
+use strum::FromRepr;
 const CELLSIZE:i32 = 5;
 const ITERATION:i32 = 8;
-
+const NCELLSEARCHRANGE: usize = 3;
+const NCELLNEIGHTBOR: usize = NCELLSEARCHRANGE*NCELLSEARCHRANGE;
 #[derive(Component)]
 struct Cell {
     x: i32,
     y: i32,
-    value: MapCellType
+    value: MapCellType,
+    neightbors: Vec<Entity>
 }
 
 fn hash_coord(x:i32,y:i32) -> String {
     format!("{x}_{y}")
 }
 
-#[derive(Debug, Clone, Copy, Eq, PartialEq, Hash)]
+#[derive(Default,Debug, Clone, Copy, Eq, PartialEq, Hash, FromRepr)]
 enum MapCellType {
+    #[default]
     Undeclared,
     Moutains,
     Forest,
@@ -30,24 +33,48 @@ enum MapCellType {
     HighMountains
 }
 
+const MAPCELLTABLE: [[usize;7];7] = [
+    [0,0,0,0,0,0,1],
+    [0,0,0,0,0,0,1],
+    [0,0,0,0,0,0,1],
+    [0,0,0,0,0,0,1],
+    [0,0,0,0,0,0,1],
+    [0,0,0,0,0,0,1],
+    [0,0,0,0,0,0,1]
+];
+
+fn map_table(cell_type: MapCellType, other_cell_type: MapCellType) -> usize {
+    MAPCELLTABLE[cell_type.index()][other_cell_type.index()]
+}
+
 fn get_cell_num_type() -> i32 {
     7
 }
 
-fn find_cell_shade(cell_type:MapCellType) -> Color {
-    let cell_color = match cell_type {
-        MapCellType::Undeclared => "000000",
-        MapCellType::Moutains => "AEC2B6",
-        MapCellType::Forest => "609966",
-        MapCellType::HighMountains => "BDCDD6",
-        MapCellType::Plains => "BBD6B8",
-        MapCellType::DeepWater => "93BFCF",
-        MapCellType::Water => "6096B4",
-    };
-    Color::from(Srgba::hex(cell_color).unwrap_or_default())
+impl MapCellType {
+    pub fn index(&self) -> usize {
+        *self as usize
+    }
+
+    pub fn check_conflict(&self, other_cell_type: MapCellType) -> usize {
+        map_table(*self, other_cell_type)
+    }
+
+    pub fn color(&self) -> Color {
+        let cell_color = match self {
+            MapCellType::Undeclared => "000000",
+            MapCellType::Moutains => "AEC2B6",
+            MapCellType::Forest => "609966",
+            MapCellType::HighMountains => "BDCDD6",
+            MapCellType::Plains => "BBD6B8",
+            MapCellType::DeepWater => "93BFCF",
+            MapCellType::Water => "6096B4",
+        };
+        Color::from(Srgba::hex(cell_color).unwrap_or_default())
+    }
 }
 
-#[derive(Resource, Default)]
+#[derive(Resource, Default, Clone)]
 struct Map {
     width: i32,
     height: i32,
@@ -59,8 +86,10 @@ fn main() {
         .add_plugins(DefaultPlugins)
         .add_systems(Startup, (
             setup,
+            find_neighbors_cell,
+            find_least_cellconflict,
             draw_map
-        ))
+        ).chain())
         .run();
 }
 
@@ -84,7 +113,8 @@ fn setup(
                     let cell = Cell {
                         x:x,
                         y:y,
-                        value:MapCellType::Undeclared
+                        value:MapCellType::Undeclared,
+                        neightbors:Vec::new()
                     };
                     let cell_entity = commands.spawn((cell));
                     let cell_hash = hash_coord(x, y);
@@ -102,28 +132,48 @@ fn draw_map()
 
 }
 
-fn find_least_conflict(
+fn find_neighbors_cell(
+    mut query: Query<(&mut Cell)>,
+    map: Res<Map>,
+) {
+    for (mut cell) in query.iter_mut() {
+        let x = cell.x;
+        let y = cell.y;
+        let search_range = 3;
+        for dx in (-search_range..search_range) {
+            for dy in (-search_range..search_range) {
+                let tx = (dx + x + map.width) % map.width;
+                let ty = (dy + y + map.height) % map.height;
+                let cell_hash = hash_coord(tx, ty);
+                let cell_entity = map.cells[&cell_hash];
+                cell.neightbors.push(cell_entity);
+            }
+        }
+    }
+}
+
+fn find_least_cell_conflict(
     mut commands: Commands,
-    mut query: Query<&mut Cell>,
+    mut query: Query<(&mut Cell)>,
     mut map: ResMut<Map>,
 ) -> bool {
     let success = false;
 
-    for mut cell in &mut query
+    for i in (0..query.iter().count())
     {
         let x = rand::thread_rng().gen_range(0..map.width);
         let y = rand::thread_rng().gen_range(0..map.height);
         let cell_hash = hash_coord(x, y);
         let cell_entity = map.cells[&cell_hash];
-
-        let conflicts = check_conflicts(x, y);
+        let conflicts = check_conflict_cells(cell_entity, &query, &map);
         if conflicts > 0 {
-            let mut best_type: MapCellType;
-            let mut least_conflicts: i32 = 100;
+            let mut best_type = MapCellType::Undeclared;
+            let mut least_conflicts = 100;
             let (mut temp_terrain, mut temp_conflicts) = (MapCellType::Undeclared,0);
             for j in (0..ITERATION) {
-                temp_terrain = 1 + rand::thread_rng().gen_range(0..(get_cell_num_type()-1));
-                temp_conflicts = check_conflicts(x, y);
+                let temp_terrain_num = 1 + rand::thread_rng().gen_range(0..(get_cell_num_type()-1));
+                temp_terrain = MapCellType::from_repr(temp_terrain_num as usize).unwrap_or_default();
+                temp_conflicts = check_conflicts(x, y, map.width, map.height);
                 if (temp_conflicts < conflicts) {
                     best_type = temp_terrain;
                     least_conflicts = temp_conflicts;
@@ -138,6 +188,35 @@ fn find_least_conflict(
     success
 }
 
-fn check_conflicts(x:i32, y:i32) -> i32{
-    0
+fn check_conflict_cells(
+    check_cell: Entity,
+    query: &Query<(&mut Cell)>,
+    mut map: &ResMut<Map>) -> i32
+{
+    let mut conflicts = 0;
+    if let Ok(mut selected_cell) = query.get_mut(check_cell) {
+        let neightbors_cell_array: Result<[Entity; NCELLNEIGHTBOR], _> = selected_cell.neightbors.try_into();
+        let neightbor_cell_entities:  = neightbors_cell_array.unwrap_err();
+
+        if let Ok(mut neightbor_cells) = query.get_many_mut(neightbor_cell_entities)
+        {
+            for neightbor_cell in neightbor_cells.iter() {
+
+            }
+        }
+    }
+    conflicts
+}
+
+fn check_conflicts(x:i32, y:i32, width:i32, height:i32) -> i32{
+    let mut conflicts = 0;
+    let range = 3;
+    for dx in (-range..range) {
+        for dy in (-range..range) {
+            let tx = (dx + x + width) % width;
+            let ty = (dy + y + height) % height;
+            conflicts += 1; //Replace with MapCelType::check_conflicts
+        }
+    }
+    conflicts
 } 
